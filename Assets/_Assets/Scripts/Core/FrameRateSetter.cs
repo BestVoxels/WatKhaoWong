cdf84d3295d507b284255441d10c1250a4a8b11b
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2023-08-26 17:31:57 +0700

    Script - FrameRateSetter (for iOS only)

---
+++ Assets/_Assets/Scripts/Core/FrameRateSetter.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace WatKhaoWong.Core
+{
+    public class FrameRateSetter : MonoBehaviour
+    {
+        //#if UNITY_IOS
+        private void Start()
+        {
+            if (Application.isMobilePlatform)
+            {
+                QualitySettings.vSyncCount = 0;
+                Application.targetFrameRate = 60;
+            }
+        }
+        //#endif
+    }
+}
