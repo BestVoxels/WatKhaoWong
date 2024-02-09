5f91fb5ef640edb20b00df120d941b4f8b5009b7
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2024-02-09 11:20:35 +0700

    Implemented MPVR on Credits Popup under Settings Page (make UI Buttons and Texts display and interact properly)

---
+++ Assets/_Assets/Scripts/Settings/CreditsPopup.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.Events;
+using WatKhaoWong.Attributes;
+
+namespace WatKhaoWong.Settings
+{
+    public class CreditsPopup : Popup
+    {
+        #region --Events-- (UnityEvent)
+        //[Header("Credits Popup UI Event")]
+        #endregion
+
+
+
+        #region --Methods-- (Custom PUBLIC) ~Popup UI Buttons~
+        #endregion
+    }
+}
---
+++ Assets/_Assets/Scripts/UI/Settings/CreditsPopupUI.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+using WatKhaoWong.Settings;
+
+namespace WatKhaoWong.UI.Settings
+{
+    public class CreditsPopupUI : MonoBehaviour
+    {
+        #region --Fields-- (Inspector)
+        [Header("Popup Header UI Stuffs")]
+        [SerializeField] private Button _closeButton;
+
+        //[Header("Credits Popup UI Stuffs")]
+        #endregion
+
+
+
+        #region --Fields-- (In Class)
+        private CreditsPopup _playerCreditsPopup;
+        #endregion
+
+
+
+        #region --Methods-- (Built In)
+        private void Awake()
+        {
+            _playerCreditsPopup = GameObject.FindWithTag("Player").GetComponentInChildren<CreditsPopup>();
+
+            _closeButton.onClick.AddListener(Close);
+        }
+        #endregion
+
+
+
+        #region --Methods-- (Subscriber) ~Popup Header UI~
+        private void Close() => _playerCreditsPopup.OnCloseButtonClick();
+        #endregion
+
+
+
+        #region --Methods-- (Subscriber)
+        #endregion
+    }
+}
