7e08a9d0800201fa95fe1601bac1def35dd3ed13
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2024-11-07 18:08:02 +0700

    Implement Auspicious Calendar Page UI & MPVR

---
+++ Assets/_Assets/Scripts/UI/Homes/AuspiciousCalendarUI.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using WatKhaoWong.Homes;
+
+namespace WatKhaoWong.UI.Homes
+{
+    public class AuspiciousCalendarUI : MonoBehaviour
+    {
+        #region --Fields-- (Inspector)
+        [Header("Page Header UI Stuffs")]
+        [SerializeField] private Button _backButton;
+
+        //[Header("AuspiciousCalendar UI Stuffs")]
+        #endregion
+
+
+
+        #region --Fields-- (In Class)
+        private AuspiciousCalendar _playerAuspiciousCalendar;
+        #endregion
+
+
+
+        #region --Methods-- (Built In)
+        private void Awake()
+        {
+            _playerAuspiciousCalendar = GameObject.FindWithTag("Player").GetComponentInChildren<AuspiciousCalendar>();
+
+            _backButton.onClick.AddListener(Back);
+        }
+
+        private void Start()
+        {
+            RefreshUI();
+        }
+        #endregion
+
+
+
+        #region --Methods-- (Subscriber) ~Page Header UI~
+        private void Back() => _playerAuspiciousCalendar.OnBackButtonClick();
+        #endregion
+
+
+
+        #region --Methods-- (Subscriber)
+        private void RefreshUI()
+        {
+
+        }
+        #endregion
+    }
+}
