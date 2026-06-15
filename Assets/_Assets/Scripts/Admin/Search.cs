1a5ba660d5ef647930f56f0381c8d75b7210573d
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2026-06-15 18:28:55 +0700

    feat: create Search / Edit Member Info page with full UI implementation
    • Designed and implemented Search / Edit Member Info page
    • Ensured consistent layout, spacing, and interaction patterns across both modes
    • Prepared UI elements for integration with logic (permissions, save, validation)

---
+++ Assets/_Assets/Scripts/Admin/Search.cs
@@ -0,0 +1,21 @@
+﻿using WatKhaoWong.Attributes;
+
+namespace WatKhaoWong.Admin
+{
+    public class Search : Page
+    {
+        #region --Fields-- (Inspector)
+        #endregion
+
+
+
+        #region --Events-- (UnityEvent)
+        //[Header("Search UI Event")]
+        #endregion
+
+
+
+        #region --Methods-- (Custom PUBLIC) ~Page UI Buttons~
+        #endregion
+    }
+}
---
+++ Assets/_Assets/Scripts/UI/Admin/SearchUI.cs
@@ -0,0 +1,56 @@
+﻿using WatKhaoWong.Admin;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace WatKhaoWong.UI.Admin
+{
+    public class SearchUI : MonoBehaviour
+    {
+        #region --Fields-- (Inspector)
+        [Header("Page Header UI Stuffs")]
+        [SerializeField] private Button _backButton;
+        [SerializeField] private Button _changeLangButton;
+
+        //[Header("Search UI Stuffs")]
+        #endregion
+
+
+
+        #region --Fields-- (In Class)
+        private Search _search;
+        #endregion
+
+
+
+        #region --Methods-- (Built In)
+        private void Awake()
+        {
+            _search = GameObject.FindWithTag("Player").GetComponentInChildren<Search>();
+
+            _backButton.onClick.AddListener(Back);
+            _changeLangButton.onClick.AddListener(ChangeLang);
+        }
+
+        //private void Start()
+        //{
+        //    RefreshUI();
+        //}
+        #endregion
+
+
+
+        #region --Methods-- (Subscriber) ~Page Header UI~
+        private void Back() => _search.OnBackButtonClick();
+        private void ChangeLang() => _search.OnChangeLangButtonClick();
+        #endregion
+
+
+
+        #region --Methods-- (Subscriber)
+        //private void RefreshUI()
+        //{
+
+        //}
+        #endregion
+    }
+}
